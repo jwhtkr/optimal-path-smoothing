563f3da5ba94d3d8686674afe893ffe07c48ef9f
Greg Droge <greg.droge@usu.edu>
2020-02-07 12:08:58 -0700

    Add straight corridor world

---
+++ World/CorridorWorldStraight.m
@@ -0,0 +1,21 @@
+classdef CorridorWorldStraight < PolygonWorld
+    %PolygonWorld1 Is an instantiation of Polygon World with a three polygons
+    
+    
+    methods
+        function obj = CorridorWorldStraight()
+            % Define the vertices for various polygons
+            V1 = [-2, 10, 10, 18, 18, -2; 1.75, 1.75, 10, 10, 12, 12];
+            V2 = [-2, 14, 14, 18, 18, -2;-1.75, -1.75, 6, 6, -4, -4];
+%             V3 = [3,3.25, 3.5, 3.25;0,.25, 0, -.25];
+            
+            
+            % Create the polygon world
+%             obj = obj@PolygonWorld(V1, V2, V3);
+            obj = obj@PolygonWorld(V1, V2);
+        end
+        
+        
+    end
+end
+
