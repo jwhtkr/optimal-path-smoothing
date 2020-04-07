e484db553a34975cf64ef0ed511bd26a58b4af00
BrianMerrell <merrell.brian.t@gmail.com>
2020-04-07 17:24:25 -0600

    Agent error plots

--- Scenarios/MultiAgent_ParamOptScenario.m
+++ Scenarios/MultiAgent_ParamOptScenario.m
@@ -40,7 +40,6 @@ classdef MultiAgent_ParamOptScenario < MultiScenario
         %%%% Plotting methods - Add desired velocities %%%%
         function plotResults(obj)
             % Create the figure and get additional data
-            figure;
             
             
             % Calculate the control and velocities vs time
@@ -73,14 +72,12 @@ classdef MultiAgent_ParamOptScenario < MultiScenario
             plot([obj.tmat(1) obj.tmat(end)],[0 0], ':r', 'linewidth', 2); hold on;
             plot(obj.tmat, err_vec(1,:), 'b', 'linewidth', 2);
             ylabel('Position Error');
-            legend('Desired', 'Actual');
             
             %Plot Error
             subplot(4,1,2);
             plot([obj.tmat(1) obj.tmat(end)],[0 0], ':r', 'linewidth', 2); hold on;
             plot(obj.tmat, yaw_err_vec(1,:), 'b', 'linewidth', 2);
             ylabel('Yaw Error');
-            legend('Desired', 'Actual');
             
             % Plot v and desired
             subplot(4,1,3);
@@ -108,15 +105,15 @@ classdef MultiAgent_ParamOptScenario < MultiScenario
                 yaw_err_vec = zeros(1, t_len);
                 for k = 1:t_len
                     u = obj.ctrl(2*k-1:2*k,1);
-                    [v_vec(:,k), w_vec(:,k)] = obj.agents(i).vehicle.kinematics.getVelocities(obj.tmat(k), obj.xmat(:,k), u(:,1));
+                    [v_vec(:,k), w_vec(:,k)] = obj.agents(i).vehicle.kinematics.getVelocities(obj.tmat(k), obj.xmat(:,k,i+1), u(:,1));
                     err_v_vec(k) = obj.agents(i).trajectory.v(k+1) - v_vec(:,k);
                     err_w_vec(k) = obj.agents(i).trajectory.w(k+1) - w_vec(:,k);
                     
                     xd_vec(:,k) = obj.agents(i).trajectory.x(k+1);
                     yd_vec(:,k) = obj.agents(i).trajectory.y(k+1);
-                    err_vec(:,k) = norm([obj.agents(i).trajectory.x(k+1);obj.agents(i).trajectory.y(k+1)] - obj.xmat(1:2,k));
+                    err_vec(:,k) = norm([obj.agents(i).trajectory.x(k+1);obj.agents(i).trajectory.y(k+1)] - obj.xmat(1:2,k,i+1));
 
-                    h_e = [cos(obj.agents(i).trajectory.psi(k+1));sin(obj.agents(i).trajectory.psi(k+1))] - [cos(obj.xmat(3,k));sin(obj.xmat(3,k))];
+                    h_e = [cos(obj.agents(i).trajectory.psi(k+1));sin(obj.agents(i).trajectory.psi(k+1))] - [cos(obj.xmat(3,k,i+1));sin(obj.xmat(3,k,i+1))];
                     yaw_err_vec(:,k) = h_e'*h_e/2;
                 end
                 
@@ -126,21 +123,18 @@ classdef MultiAgent_ParamOptScenario < MultiScenario
                 plot([obj.tmat(1) obj.tmat(end)],[0 0], ':r', 'linewidth', 2); hold on;
                 plot(obj.tmat, err_vec(1,:), 'b', 'linewidth', 2);
                 ylabel('Position Error');
-                legend('Desired', 'Actual');
                 
                 %Plot Error
                 subplot(4,1,2);
                 plot([obj.tmat(1) obj.tmat(end)],[0 0], ':r', 'linewidth', 2); hold on;
                 plot(obj.tmat, yaw_err_vec(1,:), 'b', 'linewidth', 2);
                 ylabel('Yaw Error');
-                legend('Desired', 'Actual');
                 
                 % Plot v and desired
                 subplot(4,1,3);
                 plot([obj.tmat(1) obj.tmat(end)],[0 0], ':r', 'linewidth', 2); hold on;
                 plot(obj.tmat, err_v_vec, 'b', 'linewidth', 2);
                 ylabel('Vel Error');
-                %             legend('Desired', 'Actual');
                 
                 % Plot w and desired
                 subplot(4,1,4);
